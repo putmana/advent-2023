147cd1022c2b32a97dc3b581fa86cb230f449c16
putmana <adam@ptmn.io>
2023-12-16 04:32:40 -0700

    Completed part one

---
+++ day-01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = GetTotal("input.txt");
+            Console.WriteLine(total);
+        }
+
+        static int GetTotal(string path)
+        {
+            // Open and read the file
+            var lines = File.ReadAllLines(path);
+
+            // Add up the value from each line
+            return lines.Aggregate(0, (total, line) =>
+            {
+                // Convert string into array of valid integers
+                var nums = line
+                    .Where(chr => int.TryParse(chr.ToString(), out _)) // Filter out non-integer characters
+                    .Select(chr => int.Parse(chr.ToString())); // Parse character as an integer
+
+                // Return a two-digit number made of the first and last integers in the array
+                return total + (nums.First() * 10 + nums.Last());
+
+            });
+        }
+    }
+}
