dba23c1dd842913dab70b6384073ab72f9b5003d
putmana <adam@ptmn.io>
2023-12-16 16:00:12 -0700

    Completed part two

--- day-01/Program.cs
+++ day-01/Program.cs
@@ -1,14 +1,19 @@
 ﻿namespace DayOne
 {
+    using System.Text.RegularExpressions;
+
     class Program
     {
         static void Main(string[] args)
         {
             var partOne = PartOne("input.txt");
             Console.WriteLine(partOne);
+
+            var partTwo = PartTwo("input.txt");
+            Console.WriteLine(partTwo);
         }
 
-        // PART ONE
+        // ======== PART ONE ======== 
         static int PartOne(string path)
         {
             // Open and read the file
@@ -23,6 +28,44 @@
                     .Select(chr => int.Parse(chr.ToString())); // Parse character as an integer
 
                 // Return a two-digit number made of the first and last integers in the array
+                return total + (nums.First() * 10 + nums.Last());
+
+            });
+        }
+
+
+        // ======== PART TWO ========
+        static int PartTwo(string path)
+        {
+            // Open and read the file
+            var lines = File.ReadAllLines(path);
+
+            // Number lookup table
+            var lookup = new Dictionary<string, int> {
+                {"zero", 0}, {"0", 0},
+                {"one", 1}, {"1", 1},
+                {"two", 2}, {"2", 2},
+                {"three", 3}, {"3", 3},
+                {"four", 4}, {"4", 4},
+                {"five", 5}, {"5", 5},
+                {"six", 6}, {"6", 6},
+                {"seven", 7}, {"7", 7},
+                {"eight", 8}, {"8", 8},
+                {"nine", 9}, {"9", 9},
+            };
+
+            // Create a regular expression to match numbers and instances of number words
+            var pattern = @"(?<=(zero|one|two|three|four|five|six|seven|eight|nine|[0-9]))";
+
+            // Add up the values from each line
+            return lines.Aggregate(0, (total, line) =>
+            {
+                // Match line against regular expression
+                var nums = Regex.Matches(line, pattern)
+                    .Select(match => match.Groups[1].Value) // Get matching string
+                    .Select(match => lookup[match]); // Use lookup table to convert string to integer
+
+                // Return a two digit number made up of the first and last integers in the array
                 return total + (nums.First() * 10 + nums.Last());
 
             });
--- day-01/obj/Debug/net8.0/day-01.AssemblyInfo.cs
+++ day-01/obj/Debug/net8.0/day-01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day-01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0513e287a8563f7ed89434806a7de72eab16293d")]
 [assembly: System.Reflection.AssemblyProductAttribute("day-01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day-01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
